798fc805619ca578c174eefa6c45222fb673b832
Zoran Vranesevic <zoran.vranesevic@rt-rk.com>
2017-10-12 08:54:10 +0200

    edit h3 grana2

--- h3.h
+++ h3.h
@@ -1,1 +1,8 @@
 //this is h3
+/*
+#grananja
+git branch [ime_grane] - kreira granu, tacka grananja je trenutni head
+git branch [ime_grane] [ref] - kreira granu, ali je tacka grananja ref
+git checkout -b [ime_grane]/[ref] - isto kao branch, samo se automatski HEAD menja na novu granu
+git branch - info o dostupnim granama i o tome koja je aktivna
+*/
