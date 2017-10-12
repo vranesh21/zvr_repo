e2f9f7369f3e4d455fc567998e69a38030cc5ddd
Zoran Vranesevic <zoran.vranesevic@rt-rk.com>
2017-10-12 08:54:10 +0200

    edit h3 grana2

--- h3.h
+++ h3.h
@@ -10,3 +10,10 @@ git branch - info o dostupnim granama i o tome koja je aktivna
 git commit -m "komentar" - komituje promene sa komentarom
 git commit -a -m "komentar" - komituje sve fajlove koji se prate a promenjeni su (preskace se ADD)
 */
+/*
+#grananja
+git branch [ime_grane] - kreira granu, tacka grananja je trenutni head
+git branch [ime_grane] [ref] - kreira granu, ali je tacka grananja ref
+git checkout -b [ime_grane]/[ref] - isto kao branch, samo se automatski HEAD menja na novu granu
+git branch - info o dostupnim granama i o tome koja je aktivna
+*/
