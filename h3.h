9634fe2b95787df6bb97f98944541a0a41c5ae7a
Zoran Vranesevic <zoran.vranesevic@rt-rk.com>
2017-10-12 08:52:12 +0200

    edit h3. master

--- h3.h
+++ h3.h
@@ -1,1 +1,6 @@
 //this is h3
+/*
+#commit
+git commit -m "komentar" - komituje promene sa komentarom
+git commit -a -m "komentar" - komituje sve fajlove koji se prate a promenjeni su (preskace se ADD)
+*/
